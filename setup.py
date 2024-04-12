605b5e89228f232e82ba2cda45bac660b79a2117
Jordan Safer <jordansafer2@gmail.com>
2024-04-12 17:56:03 -0400

    add links

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='g2papi',
-    version='1.0.2',
+    version='1.0.3',
     author='Jordan Safer',
     author_email='genomics2proteins@gmail.com',
     description='A python client and CLI for easy access to G2P portal APIs',
     long_description=long_description,
     long_description_content_type="text/markdown",
+    project_urls={
+        "GitHub": "https://www.github.com/broadinstitute/g2papi",
+        "G2P portal": "https://g2p.broadinstitute.org"
+    },
     packages=find_packages(),
     install_requires=[
         'requests',

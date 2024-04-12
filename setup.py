39027aa14ec65e66466dc46d17768e0545c728d1
Jordan Safer <jordansafer2@gmail.com>
2024-04-12 17:41:46 -0400

    long desc

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='g2papi',
     version='1.0.0',
     author='Jordan Safer',
     author_email='genomics2proteins@gmail.com',
-    description='A tool for interacting with the G2P API.',
+    description='A python client and CLI for easy access to the Genomics to Proteins portal APIs.',
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[
         'requests',
